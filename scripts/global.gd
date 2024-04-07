553305f0b79c90e021a12beb26653f03ad9ecb0a
M-J-Laurent <jayszef7@gmail.com>
2024-04-06 19:41:44 -0700

    Added timer and level exit

--- scripts/global.gd
+++ scripts/global.gd
@@ -2,3 +2,4 @@ extends Node
 
 var passiveDrain = 1
 var player_size = 10
+var time_taken=[]
---
+++ scripts/level_exit.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+signal endLevel
+signal canEnd
+signal cannotEnd
+
+var NextLevel=0
+var achievments=0
+
+
+func _on_popup_body_entered(body):
+	if body.is_in_group("Player"):
+		if NextLevel>=achievments:
+			emit_signal("canEnd")
+		else:
+			emit_signal("cannotEnd")
+	
+		
+
+
+func _on_exit_body_entered(body):
+	if body.is_in_group("Player"):
+		if NextLevel>=achievments:
+			emit_signal("endLevel")
